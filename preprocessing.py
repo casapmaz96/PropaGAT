3cf783e7b4f8a9b82683abef16586332e0dd358d
casapmaz96 <casapmaz96@gmail.com>
2021-01-20 06:07:41 +0000

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -23,49 +23,11 @@ def preprocessor(datasetTrain, datasetTest, vocDict={'<UNK>':0}, path='dataset')
         reader = csv.reader(data, delimiter='\t')
         i = 0
 
-        #articles = [[[s+'\n' for s in sent_tokenize(a[0])], a[4]] for a in itertools.islice(reader, 10)]
-        #articleLen = [str(len(a[0]))+'\n' for a in itertools.islice(reader, 10)]
-        #articleID = [a[4]+'\n' for a in itertools.islice(]
-        #articlesTrain
         for a in reader:
-##            i+=1
             articlesTrain[a[4]] = sent_tokenize(a[0])
-#            if i==10:
+
         with open('articlesTrain.json', 'w') as artfile:
             json.dump(articlesTrain, artfile)
-##                return
-#        print(len(list(reader)))
-#O#        for a in reader:
-            #print("len: ", len(reader))
-#            caching.clear_cache()
-#O#            i+=1
-#O#            if i%100 == 1:
-#O#                st = time.time()
-#O#            voc.addWords(a[0])
-
-#O#            sentences = sent_tokenize(a[0])
-#O#            id = a[4]
-#O#            labels[id] = a[-1]
-
-#O#            f = open(path+'/train/'+id+'.txt', 'w')
-#O#            sentences = [s+'\n' for s in sentences]
-            
-#O#            f.writelines(sentences)
-            #print(*sentences, file=f)
-#O#            f.close()
-#O#            if i%100 == 0:
-#O#                et = time.time()
-#O#                print("\nArticle no. ", i , " processed.")
-#O#                print("Time taken: ", et-st)
-
-
-#            if i == 500:
-#O#        with open('vocab.json', 'w') as vocfile:
-#O#            json.dump(voc.w2i, vocfile)
-#                return
-
-#O#        voc = Vocab(vocDict)
-        #np.save('vocab.npy', voc.w2i, allow_pickle=True) #; np.save('labels.npy', labels)
 
     print("Processing the test set\n")
     articlesTest={}
@@ -76,48 +38,12 @@ def preprocessor(datasetTrain, datasetTest, vocDict={'<UNK>':0}, path='dataset')
 
     with open('articlesTest.json', 'w') as artfile:
         json.dump(articlesTest, artfile)
-##
-        #i = 0
-        #articlesTest = [sent_tokenize(a[0]) for a in reader]
 
-#O#        for a in reader:
-#            caching.clear_cache()
-#O#            i +=1
-#O#            if i%100 == 1:
-#O#                st = time.time()
-
-#O#            sentences = sent_tokenize(a[0])
-#O#            id = a[4]
-#O#            labels[id] = a[-1]
-
-#O#            f = open(path+'/test/'+id+'.txt', 'w')
-
-#            print(*sentences, file=f)
-#O#            sentences = [s+'\n' for s in sentences]
-#O#            f.writelines(sentences)
-
-#O#            f.close()
-
-#O#            if i%100 == 0:
-#O#                et = time.time()
-#O#                print("Article no. ", i , " processed.\n")
-#O#                print("Time taken: ", et-st)
-
-
-#           if i%100 == 0: print("Article no. ", i , " processed.\n")
-#O#        with open('labels.json', 'w') as labelfile:
-#O#            json.dump(labels, labelfile)
-
-    ## save voc dict
-    #np.save('vocab.npy', voc.w2i)
-    #np.save('labels.npy', labels, allow_pickle=True)
-
-#    return voc, labels
-
+        
 def sentencePreprocessor(trainPath, testPath, trainLabels, testLabels, saveTrain = 'trainSentences.json', saveTest = 'testSentences.json'):
 
-
-
+    ##The same preprocessing method as above, but for dataset annotated at sentence level
+    
     ##Create labels dictionary
     labelDict = {}
 
